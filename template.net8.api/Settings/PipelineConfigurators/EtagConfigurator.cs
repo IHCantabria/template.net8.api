cd4f0e879d1a94f2b7bee94dc1f1b3cc7a56d366
David Coterillo Gonzalez <coterillogd@unican.es>
2025-01-30 12:20:34 +0100

    Improved Etag logic

--- template.net8.api/Settings/PipelineConfigurators/EtagConfigurator.cs
+++ template.net8.api/Settings/PipelineConfigurators/EtagConfigurator.cs
@@ -1,32 +1,38 @@
 ﻿using Delta;
+using template.net8.api.Business;
 using template.net8.api.Core.Attributes;
 using template.net8.api.Domain.Persistence.Context;
 using template.net8.api.Settings.Interfaces;
 
-namespace template.net8.api.Settings.PipelineConfigurators
+namespace template.net8.api.Settings.PipelineConfigurators;
+
+/// <summary>
+///     Etag Db Tracking Configurator
+/// </summary>
+[CoreLibrary]
+public sealed class EtagConfigurator : IPipelineConfigurator
 {
     /// <summary>
-    ///     Etag Db Tracking Configurator
+    ///     Load order of the pipeline configurator
     /// </summary>
-    [CoreLibrary]
-    public sealed class EtagConfigurator : IPipelineConfigurator
+    public short LoadOrder => 4;
+
+    /// <summary>
+    ///     Configure Pipeline for the Etag Db Tracking
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    public Task ConfigurePipelineAsync(WebApplication app)
     {
-        /// <summary>
-        ///     Load order of the pipeline configurator
-        /// </summary>
-        public short LoadOrder => 4;
+        DeltaExtensions.UseResponseDiagnostics = true;
+        app.UseDelta<ProjectDbContext>(_ => BusinessConstants.ApiName);
 
-        /// <summary>
-        ///     Configure Pipeline for the Etag Db Tracking
-        /// </summary>
-        /// <param name="app"></param>
-        /// <returns></returns>
-        public Task ConfigurePipelineAsync(WebApplication app)
-        {
-            DeltaExtensions.UseResponseDiagnostics = true;
-            app.UseDelta<ProjectDbContext>();
+        //Beware of caching on permission-segregated data, use logic similar to this:
+        //app.UseDelta<ProjectDbContext>(httpContext =>
+        //    httpContext.User.Identity is not null && httpContext.User.Identity.IsAuthenticated
+        //        ? $"{BusinessConstants.ApiName}:{BusinessConstants.AuthenticatedUser}"
+        //        : $"{BusinessConstants.ApiName}:{BusinessConstants.AnonymousUser}");
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
     }
 }
